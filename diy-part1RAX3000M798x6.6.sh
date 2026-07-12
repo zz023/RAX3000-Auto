6f7b7d85f7986f1ac6c477f82dbc3d1d4867badb
zz023 <128499821+zz023@users.noreply.github.com>
2026-07-12 11:49:21 +0800

    Update diy-part1RAX3000M798x6.6.sh

--- diy-part1RAX3000M798x6.6.sh
+++ diy-part1RAX3000M798x6.6.sh
@@ -19,5 +19,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git' >>feeds.conf.default
